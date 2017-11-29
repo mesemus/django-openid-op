c6b2cd413982d004847b60ae1e5a93caf254d2f9
Mirek Simek <miroslav.simek@vscht.cz>
2017-11-29 09:44:56 +0100

    default parameters in apps, travis

--- openid_connect_op/apps.py
+++ openid_connect_op/apps.py
@@ -15,3 +15,21 @@ class OpenIDConnectOPApp(AppConfig):
             settings.OPENID_USERINFO_PROVIDERS = \
                 UserInfoProviderRegistry(getattr(settings, 'OPENID_SCOPE_CLAIMS', {}),
                                          getattr(settings, 'OPENID_CLAIM_PROVIDERS', {}))
+
+        if not hasattr(settings, 'OPENID_JWT_PRIVATE_KEY'):
+            settings.OPENID_JWT_PRIVATE_KEY = 'jwt_private.pem'
+
+        if not hasattr(settings, 'OPENID_JWT_PUBLIC_KEY'):
+            settings.OPENID_JWT_PUBLIC_KEY = 'jwt_public.pem'
+
+        if not hasattr(settings, 'OPENID_CONNECT_OP_AES_KEY'):
+            settings.OPENID_CONNECT_OP_AES_KEY = b'1234567890abcdef'
+
+        if not hasattr(settings, 'OPENID_USER_CONSENT_VIEW'):
+            settings.OPENID_USER_CONSENT_VIEW = 'test:consent'
+
+        if not hasattr(settings, 'OPENID_DEFAULT_ACCESS_TOKEN_TTL'):
+            settings.OPENID_DEFAULT_ACCESS_TOKEN_TTL = 3600
+
+        if not hasattr(settings, 'OPENID_DEFAULT_REFRESH_TOKEN_TTL'):
+            settings.OPENID_DEFAULT_REFRESH_TOKEN_TTL = 3600 * 24
