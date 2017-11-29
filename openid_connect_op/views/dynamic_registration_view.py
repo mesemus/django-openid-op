018829b9bad01c33177d9568f8faf10839bb8a18
Mirek Simek <miroslav.simek@vscht.cz>
2017-11-29 21:44:30 +0100

    disabling access token

--- openid_connect_op/views/dynamic_registration_view.py
+++ openid_connect_op/views/dynamic_registration_view.py
@@ -27,8 +27,10 @@ class DynamicClientRegistrationView(RatelimitMixin, OAuthRequestMixin, View):
     @method_decorator(csrf_exempt)
     @method_decorator(access_token_required(disabled_settings='OPENID_DYNAMIC_CLIENT_REGISTRATION_DISABLE_ACCESS_TOKEN'))
     def dispatch(self, request, *args, **kwargs):
-        if request.openid_access_token.token_type != OpenIDToken.TOKEN_TYPE_CLIENT_DYNAMIC_REGISTRATION:
+        if request.openid_access_token and \
+                        request.openid_access_token.token_type != OpenIDToken.TOKEN_TYPE_CLIENT_DYNAMIC_REGISTRATION:
             return HttpResponseForbidden('Can not use this token to access dynamic registration view')
+
         if request.method != 'POST':
             return HttpResponseBadRequest('Only POST is allowed')
 
