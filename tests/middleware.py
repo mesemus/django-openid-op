c9572efbf29a2b5ad67a85ae87af761f4554bb5a
Mirek Simek <miroslav.simek@vscht.cz>
2017-12-01 22:37:23 +0100

    debugging meta...

--- tests/middleware.py
+++ tests/middleware.py
@@ -20,7 +20,7 @@ class RequestLoggerMiddleware(object):
             print('    path    :', request.path)
             print('    method  :', request.method)
             print('    AUTH    :', request.META.get('HTTP_AUTHORIZATION'))
-            print('    META    :', list(request.META.keys()))
+            print('    META    :', list(sorted(request.META.keys())))
             print('    GET     :', request.GET)
             print('    POST    :', request.POST)
             if _body_to_log:
