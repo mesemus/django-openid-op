5cfe4f2e46d2b5c452def0dba88d219fbc956907
Mirek Simek <miroslav.simek@vscht.cz>
2018-01-03 22:40:41 +0100

    private_key_jwt support - fix for no 'typ' in assertion

--- openid_connect_op/utils/jwt.py
+++ openid_connect_op/utils/jwt.py
@@ -117,7 +117,7 @@ class JWTTools:
 
         header, __ = jwt.process_jwt(token)
         key = client.get_key(alg=header.get('alg', 'RS256'), kid=header.get('kid', None))
-        return jwt.verify_jwt(token, key, [key._params.get('alg', 'RS256')])
+        return jwt.verify_jwt(token, key, [key._params.get('alg', 'RS256')], checks_optional=False)
 
     @staticmethod
     def unverified_jwt_payload(token):
